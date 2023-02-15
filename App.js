5741a87581ceec80f8c307a72a147e5a825fb99a
Shreya <109687884+Shreya-R-2003@users.noreply.github.com>
2023-02-15 12:55:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from 'react';
+
+const UserLocationSearch = () => {
+  const [userLocation, setUserLocation] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setUserLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      (error) => console.error(error)
+    );
+  }, []);
+
+  const handleSearch = async () => {
+    const response = await fetch(`http://localhost:8800/search/location?lat=${userLocation.lat}&lng=${userLocation.lng}`, {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+
+    })
+    .then((response) => response.json())
+    .then((messages) => {console.log("yes");})
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <p>User location: {userLocation.lat}, {userLocation.lng}</p>
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default UserLocationSearch;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import React, { useState, useEffect } from "react";
+// // import "./App.css";
+
+// // function App() {
+// //   const [message, setMessage] = useState("");
+
+// //   useEffect(() => {
+// //     fetch("http://localhost:8800/message")
+// //       .then((res) => res.json())
+// //       .then((data) => setMessage(data.message));
+// //   }, []);
+
+// //   return (
+// //     <div className="App">
+// //       <h1>{message}</h1>
+// //     </div>
+// //   );
+// // }
+// // //here
+// // import React, { useState, useEffect } from 'react';
+
+// // const UserLocationSearch = () => {
+// //   const [userLocation, setUserLocation] = useState({});
+
+// //   useEffect(() => {
+// //     navigator.geolocation.getCurrentPosition(
+// //       (position) => {
+// //         setUserLocation({
+// //           lat: position.coords.latitude,
+// //           lng: position.coords.longitude,
+// //         });
+// //       },
+// //       (error) => console.error(error)
+// //     );
+// //   }, []);
+
+// //   return (
+// //     <div>
+// //       <p>User location: {userLocation.lat}, {userLocation.lng}</p>
+// //     </div>
+// //   );
+// // };
+
+
+// // export default UserLocationSearch;
+// // //here
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// const UserLocationSearch = () => {
+//   const [userLocation, setUserLocation] = useState({});
+
+//   useEffect(() => {
+//     navigator.geolocation.getCurrentPosition(
+//       (position) => {
+//         setUserLocation({
+//           lat: position.coords.latitude,
+//           lng: position.coords.longitude,
+//         });
+//       },
+//       (error) => console.error(error)
+//     );
+//   }, []);
+
+//   const handleSearch = () => {
+//     axios
+//       .get(`http://localhost:8800/search/location`, {
+//         params: {
+//           lat: userLocation.lat,
+//           lng: userLocation.lng,
+//         },
+//       })
+//       .then((response) => {
+//         console.log(response.data);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   };
+
+//   return (
+//     <div>
+//       <p>User location: {userLocation.lat}, {userLocation.lng}</p>
+//       <button onClick={handleSearch}>Search</button>
+//     </div>
+//   );
+// };
+
+// export default UserLocationSearch;
